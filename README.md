8bc63429742b31977a0bf9c8aa8eabd715e65b64
mustafaduyarer <mustafaduyarer@gmail.com>
2024-06-29 00:45:53 +0200

    portfolio created

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React/Redux/Router Starter Project
+# mustafa-duyarer-portfolio-react Project
 
 > Provided for the students of the [Bottega Code School](https://bottega.tech/)
 
--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import moment from 'moment/moment';
 
+import PortfolioContainer from "./portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Mustafa Duyarer DevCamp React Starter</h1>
-        <h2>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        
+        <h1>
         <div>
           {moment().format('Do MMMM YYYY, h:mm:ss a')}
         </div>
-        </h2>
+        </h1>
         
         
       </div>
