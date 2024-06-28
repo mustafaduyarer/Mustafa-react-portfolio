1e6ac6f0017c48c72b3bae999df4e4b21da2d99a
mustafaduyarer <mustafaduyarer@gmail.com>
2024-06-28 13:33:39 +0200

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import moment from 'moment/moment';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Mustafa Duyarer DevCamp React Starter</h1>
+        <h2>
+        <div>
+          {moment().format('Do MMMM YYYY, h:mm:ss a')}
+        </div>
+        </h2>
+        
         
       </div>
     );
