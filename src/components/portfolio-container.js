d457af4d8e209371fcaa956e9344568c697f9622
mustafaduyarer <mustafaduyarer@gmail.com>
2024-06-29 20:06:39 +0200

    portfolio created

--- src/components/portfolio-container.js
+++ src/components/portfolio-container.js
@@ -4,6 +4,7 @@ export default class PortfolioContainer extends Component {
   render() {
     return (
       <div>
+        <h2>Portfolio items go here ...</h2>
         <h2>Portfolio items go here updated...</h2>
       </div>
     );
