5430fd2d0a2d6a4512035c3655de61f43b241866
mustafaduyarer <mustafaduyarer@gmail.com>
2024-07-04 14:01:38 +0200

    NavLink added

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,38 @@
-import React, { Component } from 'react';
-import moment from 'moment/moment';
+import React, { Component } from "react";
+import moment from "moment/moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
-import NavigationContainer from './navigation/navigation-container';
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog.js";
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <NavigationContainer />
+      <div className="app">
+        <Router>
+          <div>
+            <NavigationContainer />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Mustafa Duyarer DevCamp React Starter</h1>
-        <PortfolioContainer/>
-        
+        <PortfolioContainer />
+
         <h1>
-        <div>
-          {moment().format('Do MMMM YYYY, h:mm:ss a')}
-        </div>
+          <div>{moment().format("Do MMMM YYYY, h:mm:ss a")}</div>
         </h1>
-        
-        
       </div>
     );
   }
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class NavigationComponent extends Component {
-    constructor(){
-        super();
-
-    }
+  constructor() {
+    super();
+  }
 
   render() {
     return (
       <div>
-        <h1>MD</h1>
-        <button>Home</button>
-        <button>About</button>
-        <button>Contact</button>
-        <button>Blog</button>
+        <NavLink exact to="/" activeClassName="nav-link-active">
+          Home
+        </NavLink>
+
+        <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
+        <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+        <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+
+       
         {false ? <button>Add Blog</button> : null}
       </div>
     );
