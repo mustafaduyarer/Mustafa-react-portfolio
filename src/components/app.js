0e85d6f787320e2a63960436aed059f07bf7114d
mustafaduyarer <mustafaduyarer@gmail.com>
2024-07-05 16:42:30 +0200

    API verileri degistirildi

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import moment from "moment/moment";
-import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -13,26 +11,9 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
-constructor(){
-  super();
 
-  this.getPortfolioItems = this.getPortfolioItems.bind(this);
-}
-
-  getPortfolioItems() {
-    axios.get("https://mustafaduyarer.devcamp.space/portfolio/portfolio_items")
-  .then(response => {
-    // handle success
-    console.log("responce data",response);
-  })
-  .catch(error=> {
-    // handle error
-    console.log(error);
-  });
-  }
-
+ 
   render() {
-   this.getPortfolioItems();
     return (
       <div className="app">
         <Router>
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"; //class based component
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 //State
@@ -8,21 +9,14 @@ export default class PortfolioContainer extends Component {
   constructor() {
     super();
     // console.log("Portfolio container has rendered");
+
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: [
-        { title: "kartal", category: "eCommerce", slug: "kartal" },
-        { title: "Mevlana", category: "Scheduling", slug: "Mevlana" },
-        { title: "aktisad", category: "Enterprise", slug: "aktisad" },
-        {
-          title: "swerige zaman",
-          category: "eCommerce",
-          slug: "swerige-zaman",
-        },
-      ],
+      data: []
     };
     this.handleFilter = this.handleFilter.bind(this);
+   
   }
 
   handleFilter(filter) {
@@ -33,19 +27,43 @@ export default class PortfolioContainer extends Component {
     });
   }
 
+  getPortfolioItems() {
+    axios.get("https://mustafaduyarer.devcamp.space/portfolio/portfolio_items")
+  .then(response => {
+    // handle success
+  
+    this.setState({
+      data: response.data.portfolio_items
+    })
+  })
+  .catch(error=> {
+    // handle error
+    console.log(error);
+  });
+  }
+
+
   portfolioItems() {
     // const data = ["kartal", "Mevlana","aktisad","swerige zaman"];
 
     return this.state.data.map((item) => {
-      return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />;
+      console.log('item data', item);
+      
+      return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.id} />;
       //return <h1>{item}</h1>
     });
+  }
+
+  componentDidMount(){
+    this.getPortfolioItems();
+
   }
 
   render() {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
+
     return (
       <div>
         <h1>{this.state.pageTitle}</h1>
