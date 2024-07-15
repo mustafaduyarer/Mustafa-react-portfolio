cc7285d29c29394cc31008c4d52e9883d9c34b46
mustafaduyarer <mustafaduyarer@gmail.com>
2024-07-16 01:48:54 +0200

    portfolio form API connection

--- src/components/app.js
+++ src/components/app.js
@@ -17,7 +17,7 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      loggedInStatus: "NOT_LOGGED_IN"
+      loggedInStatus: "NOT_LOGGED_IN",
     };
 
     this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
@@ -27,28 +27,28 @@ export default class App extends Component {
 
   handleSuccessfulLogin() {
     this.setState({
-      loggedInStatus: "LOGGED_IN"
+      loggedInStatus: "LOGGED_IN",
     });
   }
 
   handleUnsuccessfulLogin() {
     this.setState({
-      loggedInStatus: "NOT_LOGGED_IN"
+      loggedInStatus: "NOT_LOGGED_IN",
     });
   }
 
   handleSuccessfulLogout() {
     this.setState({
-      loggedInStatus: "NOT_LOGGED_IN"
+      loggedInStatus: "NOT_LOGGED_IN",
     });
   }
 
   checkLoginStatus() {
     return axios
       .get("https://api.devcamp.space/logged_in", {
-        withCredentials: true
+        withCredentials: true,
       })
-      .then(response => {
+      .then((response) => {
         const loggedIn = response.data.logged_in;
         const loggedInStatus = this.state.loggedInStatus;
 
@@ -56,15 +56,15 @@ export default class App extends Component {
           return loggedIn;
         } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
           this.setState({
-            loggedInStatus: "LOGGED_IN"
+            loggedInStatus: "LOGGED_IN",
           });
         } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
           this.setState({
-            loggedInStatus: "NOT_LOGGED_IN"
+            loggedInStatus: "NOT_LOGGED_IN",
           });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Error", error);
       });
   }
@@ -75,7 +75,11 @@ export default class App extends Component {
 
   authorizedPages() {
     return [
-      <Route path="/portfolio-manager" component={PortfolioManager} />
+      <Route
+        key="portfolio-manager"
+        path="/portfolio-manager"
+        component={PortfolioManager}
+      />,
     ];
   }
 
@@ -96,7 +100,7 @@ export default class App extends Component {
 
               <Route
                 path="/auth"
-                render={props => (
+                render={(props) => (
                   <Auth
                     {...props}
                     handleSuccessfulLogin={this.handleSuccessfulLogin}
@@ -108,10 +112,10 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
-              {this.state.loggedInStatus === "LOGGED_IN" ? (
-                this.authorizedPages()
-              ) : null}
-             
+              {this.state.loggedInStatus === "LOGGED_IN"
+                ? this.authorizedPages()
+                : null}
+
               <Route
                 exact
                 path="/portfolio/:slug"
--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioForm extends Component {
   constructor(props) {
@@ -16,18 +17,53 @@ export default class PortfolioForm extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[desccription]", this.state.desccription);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.position);
+
+    
+    
+    return formData;
   }
 
   handleChange(event) {
-    console.log("handle change", event);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
+
+  handleSubmit(event) {
+    axios
+      .post(
+        "https://mustafaduyarer.devcamp.space/portfolio/portfolio_items",
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(error => {
+        console.log("portfolio form handleSubmit error", error);
+      });
+
+    event.preventDefault();
+  }
+
 
   render() {
     return (
       <div>
         <h1>PortfolioForm</h1>
-        <form>
-            <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
             <input
               type="text"
               name="name"
@@ -36,7 +72,46 @@ export default class PortfolioForm extends Component {
               onChange={this.handleChange}
             />
 
-            </div>
+            <input
+              type="text"
+              name="url"
+              placeholder="URL"
+              value={this.state.url}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              name="position"
+              placeholder="Position"
+              value={this.state.position}
+              onChange={this.handleChange}
+            />
+
+            <input
+              type="text"
+              name="category"
+              placeholder="Category"
+              value={this.state.category}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              name="description"
+              placeholder="Description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <button type="submit">Save</button>
+          </div>
         </form>
       </div>
     );
