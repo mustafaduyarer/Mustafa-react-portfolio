78ff47d393ae468ec5e5cbbf7e6fd1228805d254
mustafaduyarer <mustafaduyarer@gmail.com>
2024-07-25 15:09:20 +0200

    BlogDetail

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
-render() {
-return (
-<div>
-<h1>Blog detail</h1>
-</div>
-);
-}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
+  }
+
+  getBlogItem() {
+    axios
+      .get(
+        `https:///mustafaduyarer.devcamp.space/portfolio/portfolio_blogs/${this.state
+          .currentId}`
+      )
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(error => {
+        console.log("getBlogItem error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Blog detail</h1>
+      </div>
+    );
+  }
 }
