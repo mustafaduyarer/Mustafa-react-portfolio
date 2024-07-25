d07de5255b7a4ba6ad30fac0d38301b04ec16484
mustafaduyarer <mustafaduyarer@gmail.com>
2024-07-25 16:09:55 +0200

    BlogDetail

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from "react";
 import axios from "axios";
 
@@ -14,11 +15,13 @@ export default class BlogDetail extends Component {
   getBlogItem() {
     axios
       .get(
-        `https:///mustafaduyarer.devcamp.space/portfolio/portfolio_blogs/${this.state
+        `https://mustafaduyarer.devcamp.space/portfolio/portfolio_blogs/${this.state
           .currentId}`
       )
       .then(response => {
-        console.log("response", response);
+        this.setState({
+          blogItem: response.data.portfolio_blog
+        });
       })
       .catch(error => {
         console.log("getBlogItem error", error);
@@ -30,10 +33,25 @@ export default class BlogDetail extends Component {
   }
 
   render() {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
+
     return (
-      <div>
-        <h1>Blog detail</h1>
-      </div>
-    );
+        <div className="blog-container">
+          <div className="content-container">
+            <h1>{title}</h1>
+  
+            <div className="featured-image-wrapper">
+              <img src={featured_image_url} />
+            </div>
+  
+            <div className="content">{content}</div>
+          </div>
+        </div>
+      );
+    }
   }
-}
