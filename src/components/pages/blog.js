4f947ca7c4c5cb494b40e5f884b9f5beb5235f7d
mustafaduyarer <mustafaduyarer@gmail.com>
2024-07-27 01:09:14 +0200

    removeEventListener(scroll)

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
 
-
 class Blog extends Component {
   constructor() {
     super();
@@ -16,20 +15,25 @@ class Blog extends Component {
       isLoading: true
     };
 
-
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll();
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
   }
 
-  activateInfiniteScroll() {
-    window.onscroll = () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-      ) {
-        console.log("get more posts");
-      }
-    };
+  onScroll() {
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
+    }
+
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems();
+    }
   }
 
   getBlogItems() {
@@ -38,42 +42,51 @@ class Blog extends Component {
     });
 
     axios
-      .get("https://mustafaduyarer.devcamp.space/portfolio/portfolio_blogs", {
-        withCredentials: true,
-      })
-      .then((response) => {
+      .get(
+        `https://mustafaduyarer.devcamp.space/portfolio/portfolio_blogs?page=${this
+          .state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
+      .then(response => {
+        console.log("gettting", response.data);
         this.setState({
-         blogItems: response.data.portfolio_blogs,
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
           isLoading: false
         });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log("getBlogItems error", error);
       });
   }
 
-  componentDidMount(){
+  componentWillMount() {
     this.getBlogItems();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
   }
 
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem}/>;
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
 
     return (
       <div className="blog-container">
         <div className="content-container">{blogRecords}</div>
-    
+
         {this.state.isLoading ? (
-        <div className="content-loader">
-          <FontAwesomeIcon icon="spinner" spin />
-        </div>) : null}
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
       </div>
     );
   }
 }
-
 
 export default Blog;
