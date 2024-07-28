d839a0ff1d2086cc316485b0268c4eb466a8dc31
mustafaduyarer <mustafaduyarer@gmail.com>
2024-07-28 16:18:58 +0200

    BlogForm created

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
+import BlogModal from "../modals/blog-modal";
+import { faL } from "@fortawesome/free-solid-svg-icons";
 
 class Blog extends Component {
   constructor() {
@@ -12,12 +14,27 @@ class Blog extends Component {
       blogItems: [],
       totalCount: 0,
       currentPage: 0,
-      isLoading: true
+      isLoading: true,
+      blogModalIsOpen: false,
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
     this.onScroll = this.onScroll.bind(this);
     window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+  }
+
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false,
+    });
+  }
+
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true,
+    });
   }
 
   onScroll() {
@@ -38,26 +55,25 @@ class Blog extends Component {
 
   getBlogItems() {
     this.setState({
-      currentPage: this.state.currentPage + 1
+      currentPage: this.state.currentPage + 1,
     });
 
     axios
       .get(
-        `https://mustafaduyarer.devcamp.space/portfolio/portfolio_blogs?page=${this
-          .state.currentPage}`,
+        `https://mustafaduyarer.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
         {
-          withCredentials: true
+          withCredentials: true,
         }
       )
-      .then(response => {
+      .then((response) => {
         console.log("gettting", response.data);
         this.setState({
           blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
-          isLoading: false
+          isLoading: false,
         });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("getBlogItems error", error);
       });
   }
@@ -71,12 +87,20 @@ class Blog extends Component {
   }
 
   render() {
-    const blogRecords = this.state.blogItems.map(blogItem => {
+    const blogRecords = this.state.blogItems.map((blogItem) => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
 
     return (
       <div className="blog-container">
+        <BlogModal 
+        handleModalClose={this.handleModalClose}
+        modalIsOpen={this.state.blogModalIsOpen} />
+
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>Open Modal!</a>
+        </div>
+
         <div className="content-container">{blogRecords}</div>
 
         {this.state.isLoading ? (
