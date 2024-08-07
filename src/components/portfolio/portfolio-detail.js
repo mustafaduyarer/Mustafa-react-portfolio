0d34af238dff9e29d58656c89ff26e8abfde7794
mustafaduyarer <mustafaduyarer@gmail.com>
2024-08-07 19:45:52 +0200

    PortfolioDetail

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from 'axios';
 
-export default function(props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  );
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super( props);
+  }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://mustafaduyarer.devcamp.space/portfolio/portfolio_items/${
+          this.props.match.params.slug
+        }`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("res", response);
+      })
+      .catch(error => {
+        console.log("getportfolioitem error", error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Deatil for {this.props.match.params.slug}</h2>
+      </div>
+    );
+  }
 }
